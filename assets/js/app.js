3c507ccca8566016157bdbfd79fe531999efe6c2
Dru Kelly <drukelly@gmail.com>
2019-04-02 13:54:37 -0700

    API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const API_KEY = "g6htg7nRAnHgje4mKdR6pFF8WisPUXIW";
+
   let topics = ["dog", "cat", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog"];
+  let animal = topics[0];
+  
+  let queryURL = `https://api.giphy.com/v1/gifs/search?q=${animal}&api_key=${API_KEY}&limit=10`;
+
+  if (window.fetch) {
+    fetch(queryURL, {
+      method: "GET"
+    })
+      .then(result => result.json())
+      .then(response => {
+        // console.log(`fetch => ${response}`);
+        console.log(response.data);
+      });
+  } else {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", queryURL);
+    xhr.onload = event => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          let response = JSON.parse(xhr.response);
+          console.log(`xhr => ${response}`);
+        } else {
+          console.error(xhr.responseText);
+        }
+      }
+    };
+    xhr.onerror = event => {
+      console.error(xhr.responseText);
+    };
+    xhr.send();
+  }
 
   const buttonClick = (event) => {
     if (event.target.type === "button") {
@@ -11,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let button = document.createElement("button");
     button.setAttribute("type", "button");
     button.innerHTML = topic;
-    button.classList.add("lh-copy", "mb2", "mr2");
+    button.classList.add("bn", "br3", "bg-washed-blue", "black", "dib", "dim", "f6", "input-reset", "link", "lh-copy", "mb2", "mr2", "ph2", "pointer", "pv1");
     document.querySelector("header").lastElementChild.appendChild(button);
   });
 
